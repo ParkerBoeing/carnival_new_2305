c13ec0913460ed6c09c08c24f9b048c1acfc344b
Parker Boeing <parker.boeing@gmail.com>
2023-06-13 10:13:58 -0600

    Feat: build out ride class for iteration 2

---
+++ lib/ride.rb
@@ -0,0 +1,32 @@
+class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement, :rider_log, :total_revenue
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+    @rider_log = Hash.new(0)
+    @total_revenue = 0
+  end
+
+  def board_rider(rider)
+    if rider.preferences.include?(@excitement) && rider.tall_enough?(@min_height) && rider.spending_money >= @admission_fee
+      @total_revenue += admission_fee
+      rider.spending_money -= admission_fee
+      @rider_log[rider] += 1
+    else
+      p "This visitor is unable to ride."
+    end
+  end
+
+  
+
+  def tall_enough?(height)
+    if @height >= height
+      true
+    else
+      false
+    end
+  end
+end
