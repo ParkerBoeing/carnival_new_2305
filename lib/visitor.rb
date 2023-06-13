485f4f823079994d5a08e520ec7adbfbee5982a5
Parker Boeing <parker.boeing@gmail.com>
2023-06-13 10:48:49 -0600

    Refactor: remove attr accessor and use getter method instead

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,6 +1,5 @@
 class Visitor
   attr_reader :name, :height, :spending_money, :preferences
-  attr_accessor :spending_money
 
   def initialize(name, height, spending_money)
     @name = name
@@ -19,5 +18,9 @@ class Visitor
     else
       false
     end
+  end
+
+  def spending_money=(val)
+    @spending_money = val
   end
 end
